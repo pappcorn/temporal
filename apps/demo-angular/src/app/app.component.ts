7d9b779191adc7e1194482574aa5d2d9cdce3977
Jose Vargas ✈️/ <ruedagato@gmail.com>
2021-11-23 14:15:38 -0500

    Initial commit

---
+++ apps/demo-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pappcorn-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo-angular';
+}
